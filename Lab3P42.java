84054686071b3ce23fd5ec4de36af1e91883dfae
Usha-25-code <balaushasree29@gmail.com>
2025-09-17 19:50:09 +0530

    Add files via upload

---
+++ Lab3P42.java
@@ -0,0 +1,10 @@
+class Lab3P42 extends Lab3P41{
+	String name;
+	@Override
+	public void demonstrate(){
+		System.out.println("Iam method in Lab3P42 ");
+	}
+	Lab3P42(){
+		super.demonstrate();
+	}
+}
---
+++ Lab3P4M.java
@@ -0,0 +1,8 @@
+class Lab3P4M{
+	public static void main(String...args){
+		Lab3P42 p=new Lab3P42();
+		p.setName("ACE ENGINEERING COLLEGE");
+		p.getNme();
+		p.demonstrate();
+	}
+}
