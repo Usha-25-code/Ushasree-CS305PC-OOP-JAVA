70e3760875f5550716ea85f057eb502ada93a250
Usha-25-code <balaushasree29@gmail.com>
2025-09-17 18:35:57 +0530

    Update Lab2P4.java

--- Lab2P4.java
+++ Lab2P4.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
-class Lab2P3{
+class Lab2P4{
 	public static void main(String...args){
 		Scanner s = new Scanner(System.in);
 		int a = s.nextInt();
 		int b = s.nextInt();
 		System.out.println((a&b)+","+(a|b)+","+(a^b));
 	}
+
 }
