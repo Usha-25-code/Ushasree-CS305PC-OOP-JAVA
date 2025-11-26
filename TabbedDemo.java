1000c1f3240673485acbcdf7ff95846816242dc8
Usha-25-code <balaushasree29@gmail.com>
2025-11-26 11:36:12 +0530

    Add files via upload

---
+++ TabbedDemo.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+class TabbedDemo extends JFrame{
+	Label l;
+	TabbedDemo(){
+		super("Tabbed pane Demo");
+		JTabbedPane jtp = new JTabbedPane();
+
+		jtp.addTab("Tab1",new Label("Tabbed pane  1"));
+		jtp.addTab("Tab2",new Label("Tabbed pane 2"));
+		jtp.addTab("Tab3",new Label("Tabbed pane 3"));
+		add(jtp);
+		setVisible(true);
+		setSize(500,500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	public static void main(String...args){
+		new TabbedDemo();
+	}
+}
